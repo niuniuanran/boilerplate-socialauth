933626c564c201b0c44c74e339375c5f82a9a17e
niuniuanran <irene.anran.niu@gmail.com>
2020-07-05 13:16:49 +1200

    Add github passport strategy

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const session     = require('express-session');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const mongo = require("mongodb").MongoClient;
+const passport = require("passport");
+const GitHubStrategy = require("passport-github").Strategy;
+require("dotenv").config();
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
 mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) {
-        console.log('Database error: ' + err);
+    if (err) {
+        console.log("Database error: " + err);
     } else {
-        console.log('Successful database connection');
+        console.log("Successful database connection");
 
-        app.use(session({
-            secret: process.env.SESSION_SECRET,
-            resave: true,
-            saveUninitialized: true,
-        }));
+        app.use(
+            session({
+                secret: process.env.SESSION_SECRET,
+                resave: true,
+                saveUninitialized: true
+            })
+        );
         app.use(passport.initialize());
         app.use(passport.session());
 
@@ -35,65 +39,92 @@ mongo.connect(process.env.DATABASE, (err, db) => {
             if (req.isAuthenticated()) {
                 return next();
             }
-            res.redirect('/');
-        };
+            res.redirect("/");
+        }
 
         passport.serializeUser((user, done) => {
             done(null, user.id);
         });
 
         passport.deserializeUser((id, done) => {
-            db.collection('socialusers').findOne(
-                {id: id},
-                (err, doc) => {
-                    done(null, doc);
-                }
-            );
+            db.collection("socialusers").findOne({ id: id }, (err, doc) => {
+                done(null, doc);
+            });
         });
 
+        passport.use(new GitHubStrategy(
+            {
+                clientID: process.env.GITHUB_CLIENT_ID,
+                clientSecret: process.env.GITHUB_CLIENT_SECRET,
+                callbackURL: "https://plum-upbeat-bull.glitch.me/auth/github/callback"
+            },
+            function(accessToken, refreshToken, profile, cb) {
+                db.collection("socialusers").findAndModify(
+                    { id: profile.id },
+                    {},
+                    {
+                        $setOnInsert: {
+                            id: profile.id,
+                            name: profile.displayName || "John Doe",
+                            photo: profile.photos[0].value || "",
+                            email: profile.emails[0].value || "No public email",
+                            created_on: new Date(),
+                            provider: profile.provider || ""
+                        },
+                        $set: {
+                            last_login: new Date()
+                        },
+                        $inc: {
+                            login_count: 1
+                        }
+                    },
+                    { upsert: true, new: true },
+                    (err, doc) => {
+                        return cb(null, doc.value);
+                    }
+                );
+            }
+            )
+        );
+        app.get("/auth/github", (req, res) => {
+            passport.authenticate("github");
+        });
 
-        /*
-        *  ADD YOUR CODE BELOW
-        */
-        app.get("/auth/github", (req, res)=>{
-            passport.authenticate('github');
-        })
-
-        app.route('/auth/github/callback')
-            .get(passport.authenticate('github', {failureRedirect: '/'}),
+        app
+            .route("/auth/github/callback")
+            .get(
+                passport.authenticate("github", { failureRedirect: "/" }),
                 (req, res) => {
-                    res.redirect('/profile');
-                });
-
+                    res.redirect("/profile");
+                }
+            );
 
         /*
-        *  ADD YOUR CODE ABOVE
-        */
+         *  ADD YOUR CODE ABOVE
+         */
 
+        app.route("/").get((req, res) => {
+            res.render(process.cwd() + "/views/pug/index");
+        });
 
-        app.route('/')
-            .get((req, res) => {
-                res.render(process.cwd() + '/views/pug/index');
-            });
+        app.route("/profile").get(ensureAuthenticated, (req, res) => {
+            res.render(process.cwd() + "/views/pug/profile", { user: req.user });
+        });
 
-        app.route('/profile')
-            .get(ensureAuthenticated, (req, res) => {
-                res.render(process.cwd() + '/views/pug/profile', {user: req.user});
-            });
-
-        app.route('/logout')
-            .get((req, res) => {
-                req.logout();
-                res.redirect('/');
-            });
+        app.route("/logout").get((req, res) => {
+            req.logout();
+            res.redirect("/");
+        });
 
         app.use((req, res, next) => {
-            res.status(404)
-                .type('text')
-                .send('Not Found');
+            res
+                .status(404)
+                .type("text")
+                .send("Not Found");
         });
 
         app.listen(process.env.PORT || 3000, () => {
             console.log("Listening on port " + process.env.PORT);
         });
-    }});
+    }
+});
